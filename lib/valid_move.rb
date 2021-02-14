d7251fdaca02890f0895ab034d7a409bddca1e03
George Serna <sernageorge@gmail.com>
2021-02-14 05:06:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,10 +11,6 @@ def position(input)
   input.to_i-1
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, index)
-  if board[index] == ("X") || board[index] == ("O")
-    return true
-  elsif board[index] == (" ") || ("") || (nil)
-    return true
-  end
+def position_taken?(input)
+  position(input) == ("X") || position(input) == ("O")
 end
